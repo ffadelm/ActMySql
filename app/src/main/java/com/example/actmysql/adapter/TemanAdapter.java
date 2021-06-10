19b44758a5a40f7715c4e0f4e3977427b1287027
ffadelm <faysalfadelm0107@gmail.com>
2021-06-10 12:43:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/actmysql/adapter/TemanAdapter.java
@@ -0,0 +1,76 @@
+package com.example.actmysql.adapter;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.actmysql.R;
+import com.example.actmysql.database.Teman;
+
+import java.util.ArrayList;
+
+public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
+
+    private ArrayList<Teman> listData;
+
+    public TemanAdapter(ArrayList<Teman> listData){
+        this.listData = listData;
+    }
+
+    @Override
+    public TemanAdapter.TemanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
+        View view = layoutInf.inflate(R.layout.row_data_teman, parent, false);
+        return new TemanViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TemanAdapter.TemanViewHolder holder, int position) {
+        String id, nama, telpon;
+
+        id = listData.get(position).getId();
+        nama = listData.get(position).getNama();
+        telpon = listData.get(position).getTelpon();
+
+        holder.tvNama.setTextColor(Color.BLACK);
+        holder.tvNama.setText(nama);
+        holder.tvNama.setTextSize(20);
+        holder.tvTelpon.setText(telpon);
+
+        holder.cardKu.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return (listData != null)?listData.size() : 0;
+    }
+
+    public class TemanViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardKu;
+        private TextView tvNama, tvTelpon;
+        public TemanViewHolder(View view) {
+            super(view);
+            cardKu = view.findViewById(R.id.kartuKu);
+            tvNama = view.findViewById(R.id.textNama);
+            tvTelpon = view.findViewById(R.id.textTelpon);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/actmysql/database/Teman.java
@@ -0,0 +1,40 @@
+package com.example.actmysql.database;
+
+import java.io.Serializable;
+
+public class Teman implements Serializable{
+    String id,nama,telpon;
+
+    public Teman(String id, String nama, String telpon) {
+        this.id = id;
+        this.nama = nama;
+        this.telpon = telpon;
+    }
+
+    public Teman() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTelpon() {
+        return telpon;
+    }
+
+    public void setTelpon(String telpon) {
+        this.telpon = telpon;
+    }
+}
